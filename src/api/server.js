d172caa5cdee47d4b8e60c494c00a0238e2a1163
bereilhp <bereilhp@gmail.com>
2024-02-20 11:00:50 +0100

    openAPI and post route to patients

--- src/api/server.js
+++ src/api/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { connectToDatabase } = require("../db/connectAtlas");
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
 const PORT = 3456;
 
 app.get("/patients", async function (req, res) {
@@ -8,11 +11,26 @@ app.get("/patients", async function (req, res) {
     const db = await connectToDatabase();
     const collection = db.collection("patients");
     const patients = await collection.find().toArray();
-    console.log("API call made to /patients endpoint");
+    console.log("GET made to /patients endpoint");
     res.json(patients);
   } catch (error) {
     console.error("Error fetching patients:", error);
     res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/patients", async function (req, res) {
+  try {
+      const db = await connectToDatabase();
+      const collection = db.collection("patients");
+      const patient = req.body;
+      const result = await collection.insertOne(patient);
+      console.log("POST made to /patients endpoint");
+      console.log("Patient added");
+      res.status(201).json(result); 
+  } catch (error) {
+      console.error("Error adding patient:", error);
+      res.status(500).json({ error: "Internal server error" });
   }
 });
 
