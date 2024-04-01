198e6fc03cec5bc5eced8fc28159d717d548a2e8
bereilhp <bereilhp@gmail.com>
2024-04-01 21:16:08 +0200

    Reuse DB and prettierrc

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const patientRoutes = require('./routes/patients');
+const express = require("express");
+const bodyParser = require("body-parser");
+const patientRoutes = require("./routes/patients");
+const morgan = require("morgan");
 
 const app = express();
 const PORT = 3456;
 
 app.use(bodyParser.json());
+app.use(morgan("dev"));
 
-app.use('/patients', patientRoutes);
+app.use("/patients", patientRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- src/db/connectAtlas.js
+++ src/db/connectAtlas.js
@@ -1,19 +1,22 @@
-require('dotenv').config();
-const { MongoClient } = require('mongodb');
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 
 const uri = process.env.MONGO_ATLAS_URI;
-const client = new MongoClient(uri);
-
-const dbName = 'fhir-server';
+let db = null;
 
 async function connectToDatabase() {
+  if (db) {
+    return db;
+  }
+
   try {
+    const client = new MongoClient(uri);
     await client.connect();
-    console.log('Connected successfully to MongoDB Atlas');
-    const db = client.db(dbName);
+    console.log("Connected successfully to MongoDB Atlas");
+    db = client.db("fhir-server");
     return db;
   } catch (error) {
-    console.error('Error connecting to MongoDB Atlas:', error);
+    console.error("Error connecting to MongoDB Atlas:", error);
     throw error;
   }
 }
