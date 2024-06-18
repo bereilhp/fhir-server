469eebe85b5d71c9fa691fc03d4bade1c2a71b04
bereilhp <bereilhp@gmail.com>
2024-06-18 16:49:02 +0200

    Dynamic route params

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const appointmentRoutes = require("./routes/appointment");
+const dynamicRoutes = require("./routes/dynamic");
 const morgan = require("morgan");
 
 const app = express();
 const PORT = 3456;
 
 app.use(bodyParser.json());
-app.use(morgan("tiny"));
+app.use(morgan("combined"));
 
-app.use("/appointment", appointmentRoutes);
+app.use("/Appointment", appointmentRoutes);
+app.use("/dynamic", dynamicRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ src/routes/dynamic.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/api/:resource", (req, res) => {
+  const resource = req.params.resource;
+
+  console.log("Host URL: ", req.originalUrl);
+  console.log("Origin: ", req.headers.host);
+
+  if (resource === "Patients") {
+    return res
+      .status(200)
+      .json({ message: "You requested the data of Patients." });
+  } else if (resource === "Observations") {
+    return res
+      .status(200)
+      .json({ message: "You requested the data of Observations." });
+  } else {
+    return res.status(404).json({ error: "Resource not found" });
+  }
+});
+
+module.exports = router;
